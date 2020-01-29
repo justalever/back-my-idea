1c889aa6e2659230a5c1e8ed4ff4202fb9599484
Andy Leverenz <andy@justalever.com>
2020-01-29 17:04:49 -0600

    Add Pay gem tables and setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  include Pay::Billable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:stripe_connect]
   has_many :projects, dependent: :destroy
