7357629a661f6e9764ed01d44701fd793132acd2
Andy Leverenz <andy@justalever.com>
2020-01-30 17:01:59 -0600

    Fix up migration

--- db/migrate/20200130225723_add_perk_subscriptions_to_users.rb
+++ db/migrate/20200130225723_add_perk_subscriptions_to_users.rb
@@ -1,5 +1,5 @@
 class AddPerkSubscriptionsToUsers < ActiveRecord::Migration[6.0]
   def change
-    add_column :users, :perk_subscriptions, :text
+    add_column :users, :perk_subscriptions, :text, array:true, default: []
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -112,7 +112,7 @@ ActiveRecord::Schema.define(version: 2020_01_30_225723) do
     t.string "card_exp_month"
     t.string "card_exp_year"
     t.string "card_type"
-    t.text "perk_subscriptions"
+    t.text "perk_subscriptions", default: [], array: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
