e38c7195d1b4d2b1a39eb866b8c863f0d40c568c
Andy Leverenz <andy@justalever.com>
2020-04-25 12:44:12 -0500

    Update syntax

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,5 +1,5 @@
 Rails.configuration.stripe = {
-  :publishable_key => Rails.application.credentials.dig(:stripe)[:public_key],
-  :secret_key => Rails.application.credentials.dig(:stripe)[:private_key]
+  :publishable_key => Rails.application.credentials.dig(:stripe, :public_key),
+  :secret_key => Rails.application.credentials.dig(:stripe, :private_key)
 }
-Stripe.api_key = Rails.application.credentials.dig(:stripe)[:private_key]
+Stripe.api_key = Rails.application.credentials.dig(:stripe, :private_key)
