c7b04c4f0b770717ea9124a72ff6e514172c11be
Andy Leverenz <andy@justalever.com>
2020-04-25 12:46:48 -0500

    Update syntax

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -296,5 +296,5 @@ Devise.setup do |config|
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
-  config.omniauth :stripe_connect, Rails.application.credentials.dig(:stripe)[:connect_client_id], Rails.application.credentials.dig(:stripe)[:private_key]
+  config.omniauth :stripe_connect, Rails.application.credentials.dig(:stripe, :connect_client_id), Rails.application.credentials.dig(:stripe, :private_key)
 end
